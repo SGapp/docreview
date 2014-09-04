b614286278ec7a17231f1b6213840ad594726386
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-09-04 15:48:09 +0200

    abby works

--- app/services/assets/contract.rb
+++ app/services/assets/contract.rb
@@ -2,13 +2,34 @@ class Contract
   attr_reader :reader, :content
 
   def initialize(text_path)
-    @reader = PDF::Reader.new(text_path)
+    begin
+      @reader = PDF::Reader.new(text_path)
+      if content.empty?
+        puts '*'*50
+        puts "abby"
+        puts '*'*50
+        @reader = Abby::Doc.new(text_path)
+      end
+      puts '*'*50
+      puts "reader"
+      puts '*'*50
+    rescue
+      @reader = Abby::Doc.new(text_path)
+      puts '*'*50
+      puts "abby"
+      puts '*'*50
+    end
   end
 
   def content
-    @content ||= doc_pages(reader).join("\n")
+    case @reader.class.name
+    when "PDF::Reader"
+     @content ||= doc_pages(@reader).join("\n")
                                   .gsub(/[\n]+/, "\n")
                                   .gsub(/(?<=[^\.:!?-])([\n]+)(?=([^A-Z\d]))/m, " ")
+    when "Abby::Doc"
+      @content = @reader.recognized_text
+    end
   end
 
   def articles
